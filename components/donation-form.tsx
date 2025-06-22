a53b28a98798303f275fa0ac585b3c32834a736f
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-06-22 10:06:13 +0530

    changes

--- components/donation-form.tsx
+++ components/donation-form.tsx
@@ -7,7 +7,7 @@ import { Label } from "@/components/ui/label"
 
 export default function DonationForm() {
   const amountRef = useRef<HTMLInputElement>(null)
-  const upiId = "9630048447@ybl" // <-- Replace with your actual UPI ID
+  const upiId = "narendraojha997@ybl" // <-- Replace with your actual UPI ID
   const [showTxnField, setShowTxnField] = useState(false)
 
  const handleUPIClick = (e: React.MouseEvent) => {
